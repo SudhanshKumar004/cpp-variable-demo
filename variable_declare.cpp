e584d8cdcba2943accc1881405968b1b71bdd7ad
Sudhansh Kumar <sudhanshkb04@gmail.com>
2024-08-13 17:03:52 +0530

    first commit

---
+++ variable_declare.cpp
@@ -0,0 +1,16 @@
+//WAP to declare two variables roll_no and total_marks of type int,
+//and initialize them with values of your choice. 
+// and print the values on the console. 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int roll_no = 21;
+    int total_marks = 500;
+
+    cout << "roll no = " << roll_no << "\n";
+    cout << "total marks = " << total_marks << "\n";
+
+    return 0;
+}
